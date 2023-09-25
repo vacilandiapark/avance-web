0510d023478b8a820e3d77997ad884411154557c
Michael <michaeld@vacilandia.com.pe>
2023-09-25 18:01:31 -0500

    update

--- script.js
+++ script.js
@@ -11,37 +11,3 @@ document.addEventListener("DOMContentLoaded", function () {
     menuPanel.classList.remove("open");
   });
 });
-
-
-/* WHATSAPP */
-
-// Obtener elementos del DOM
-const button = document.getElementById('whatsapp-icon');
-const chatBox = document.getElementById('whatsapp-chat');
-const closeButton = document.getElementById('whatsapp-close');
-const sendButton = document.getElementById('whatsapp-send');
-const messageInput = document.getElementById('whatsapp-message-input');
-
-// Función para abrir o cerrar el chat
-function toggleChat() {
-  chatBox.classList.toggle('open');
-}
-
-// Función para enviar el mensaje a WhatsApp
-function sendMessage() {
-  const phoneNumber = '+51913479416'; // Reemplaza con tu número de teléfono
-  const message = messageInput.value;
-  const encodedMessage = encodeURIComponent(message);
-  const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
-
-  window.location.href = whatsappURL;
-}
-
-// Evento click en el botón de WhatsApp
-button.addEventListener('click', toggleChat);
-
-// Evento click en el botón de cerrar
-closeButton.addEventListener('click', toggleChat);
-
-// Evento click en el botón de enviar
-sendButton.addEventListener('click', sendMessage);
