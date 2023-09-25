d214d9b9f413ca872342cf0ecf65dc45a0792230
Michael <michaeld@vacilandia.com.pe>
2023-09-25 12:21:23 -0500

    update

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ document.addEventListener("DOMContentLoaded", function () {
         menuPanel.classList.remove("open");
     });
 });
+
+
+
+const swiperEl = document.getElementByIdSelector('carrusel')
+    Object.assign(swiperEl, {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      },
+    });
+    swiperEl.initialize();
