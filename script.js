df884dfb461277e67400fc8ec4a1952d0b2e2fb0
Michael <michaeld@vacilandia.com.pe>
2023-09-27 12:34:00 -0500

    update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const hamburguer = document.querySelector(".hamburguer");
   const menuPanel = document.getElementById("menuPanel");
