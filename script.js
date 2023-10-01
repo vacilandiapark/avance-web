9fd8e08c5b43afcf40fbbcc10200e8178c329b36
Michael <michaeld@vacilandia.com.pe>
2023-10-01 17:49:39 -0500

    update

--- script.js
+++ script.js
@@ -55,3 +55,96 @@ window.addEventListener("scroll", () => {
   const scaleX = 1 + progress / 100; // Aumenta el ancho de 1 a 2
   document.querySelector(".row-gradient").style.transform = `scaleX(${scaleX})`;
 });
+
+
+
+
+
+// PREGUNTAS Y RESPUESTAS //
+document.addEventListener('DOMContentLoaded', function () {
+  const faqItems = document.querySelectorAll('.faq-item');
+
+  faqItems.forEach(item => {
+    const question = item.querySelector('.question');
+    const answerWrapper = item.querySelector('.answer-wrapper');
+
+    item.classList.add('closed'); // Agregamos la clase "closed" por defecto
+
+    question.addEventListener('click', () => {
+      if (!item.classList.contains('active')) {
+        // Abrir la pregunta
+        closeAllQuestions();
+        item.classList.add('active');
+        item.classList.remove('closed');
+        answerWrapper.style.maxHeight = answerWrapper.scrollHeight + 'px';
+      } else {
+        // Cerrar la pregunta
+        item.classList.remove('active');
+        item.classList.add('closed');
+        answerWrapper.style.maxHeight = '0';
+      }
+    });
+  });
+
+  function closeAllQuestions() {
+    faqItems.forEach(item => {
+      const answerWrapper = item.querySelector('.answer-wrapper');
+      if (item.classList.contains('active')) {
+        item.classList.remove('active');
+        item.classList.add('closed');
+        answerWrapper.style.maxHeight = '0';
+      }
+    });
+  }
+});
+
+
+
+
+// Obtén elementos del DOM
+const images = document.querySelectorAll('.galeria .image img');
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modal-img');
+const modalContent = document.querySelector('.modal-content');
+const closeModal = document.getElementById('close-modal');
+
+// Función para mostrar el modal
+function openModal(imgSrc) {
+  // Restablecer la escala a su valor inicial
+  modalContent.style.transform = 'scale(0.8)';
+
+  modal.style.display = 'block';
+  modal.classList.add('active'); // Agregar la clase "active" para mostrar la modal
+  modalImg.src = imgSrc;
+
+  // Retrasar ligeramente la aplicación de la escala para permitir la transición
+  setTimeout(() => {
+    modalContent.style.transform = 'scale(1)';
+  }, 0);
+}
+
+// Función para cerrar el modal
+function closeModalFunction() {
+  modal.style.display = 'none';
+  modal.classList.remove('active'); // Remover la clase "active" para ocultar la modal
+  modalContent.style.transform = 'scale(0.8)'; // Restablecer la escala
+}
+
+// Agrega un controlador de clic a cada imagen
+images.forEach((image) => {
+  image.addEventListener('click', () => {
+    openModal(image.src);
+  });
+});
+
+// Agrega un controlador de clic para cerrar el modal
+closeModal.addEventListener('click', () => {
+  closeModalFunction();
+});
+
+// Cierra el modal si se hace clic en cualquier parte de la pantalla
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    closeModalFunction();
+  }
+});
