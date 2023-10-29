acca052db670b0a29c49e63f50fb456341c1dcae
Michael <michaeld@vacilandia.com.pe>
2023-10-29 17:40:41 -0500

    update

--- script.js
+++ script.js
@@ -1,5 +1,75 @@
+function delay(n) {
+  n = n || 2000;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+}
+
+function getRandomColor() {
+  const colors = ["#EC2127", "#EA0B8B", "#F58220", "#FFD200", "#77C043", "#2BABE2", "#724C9F", "#432F87"];
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+}
 
 
+function pageTransition() {
+  var tl = gsap.timeline();
+  tl.to(".loading-screen", {
+    duration: 1.2,
+    width: "100%",
+    left: "0%",
+    ease: "Expo.easeInOut",
+    backgroundColor: getRandomColor() // Cambia el color de fondo aleatoriamente entre tus colores
+  });
+
+  tl.to(".loading-screen", {
+    duration: 1,
+    width: "100%",
+    left: "100%",
+    ease: "Expo.easeInOut",
+    delay: 0.3
+  });
+  tl.set(".loading-screen", { left: "-100%" });
+}
+
+
+
+function contentAnimation() {
+  var tl = gsap.timeline();
+  tl.from(".animate-this", {
+    duration: 1, y: 30, opacity: 0, stagger: 0.4, delay: 0.2
+  });
+}
+
+$(function () {
+  barba.init({
+    sync: true,
+
+    transitions: [
+      {
+
+        async leave(data) {
+          const done = this.async();
+
+          pageTransition();
+          await delay(1000);
+          done();
+        },
+
+        async enter(data) {
+          contentAnimation();
+        },
+
+        async once(data) {
+          contentAnimation();
+        },
+
+      },
+    ],
+  });
+});
 
 
 
